4b4866ac7512f5721f1a2610950444d5ff87dd77
Nemanja Mac <makuljevicn@gmail.com>
2023-11-01 10:32:05 +0100

    wraping in try/catch-handle errors and refactoring if/else to switch

--- app.js
+++ app.js
@@ -5,35 +5,54 @@ const inputCity = document.getElementById('inputCity')
 const inputBtn = document.getElementById('inputBtn')
 const weatherIcon = document.getElementById('weatherIcon')
 let weatherDisplay = document.getElementById('weather')
+let errorDisplay = document.getElementById('error')
 
 async function checkWeather(cityName) {
     const response = await fetch(apiUrl + `&q=${cityName}` + `&appid=${apiKey}`)
 
-    const data = await response.json()
+    try {
+        const data = await response.json()
 
-    temp.innerHTML = Math.round(data.main.temp) + "°C";
-    city.innerHTML = data.name;
-    humidity.innerHTML = data.main.humidity + "%";
-    wind.innerHTML = data.wind.speed + "km/h";
-    // code block to update img according to response data
-    if (data.weather[0].main == "Clouds") {
-        weatherIcon.src = "images/clouds.png"
-    } else if (data.weather[0].main == "Clear") {
-        weatherIcon.src = "images/clear.png"
-    } else if (data.weather[0].main == "Drizzle") {
-        weatherIcon.src = "images/drizzle.png"
-    } else if (data.weather[0].main == "Mist") {
-        weatherIcon.src = "images/mist.png"
-    } else if (data.weather[0].main == "Snow") {
-        weatherIcon.src = "images/snow.png"
-    } else {
-        weatherIcon.src = "images/rain.png"
+        temp.innerHTML = Math.round(data.main.temp) + "°C";
+        city.innerHTML = data.name;
+        humidity.innerHTML = data.main.humidity + "%";
+        wind.innerHTML = data.wind.speed + "km/h";
+        // code block to update img according to response data
+        data.weather[0].main = "Clouds"
+        switch (data.weather[0].main) {
+            case 'Clouds':
+                weatherIcon.src = "images/clouds.png"
+                break;
+            case 'Clear':
+                weatherIcon.src = "images/clear.png"
+                break;
+            case 'Drizzle':
+                weatherIcon.src = "images/drizzle.png"
+                break;
+            case 'Mist':
+                weatherIcon.src = "images/mist.png"
+                break;
+            case 'Snow':
+                weatherIcon.src = "images/snow.png"
+                break;
+
+            default:
+                weatherIcon.src = "images/rain.png"
+                break;
+        }
+
+        errorDisplay.style.display = "none";
+        weatherDisplay.style.display = "block"
+        inputCity.value = ''
+        console.log(data)
+
+    } catch (err) {
+        errorDisplay.style.display = "block";
+        weatherDisplay.style.display = "none";
+        inputCity.value = ''
+        console.log(err)
     }
 
-
-
-
-    console.log(data)
 }
 
 inputBtn.addEventListener('click', () => {
